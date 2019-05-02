712ae02985bc0c91fbe9dd35e20748bcaf067e5e
Stathis Dimitriadis <stathisdim@gmail.com>
2019-05-02 19:16:56 +0300

    get timestamps without gettimeofday values

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+unsigned long get_time_ms();
 
 int main(int argc, char **argv) {
 
@@ -9,5 +12,49 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *noise;
+
+    unsigned long *timestamps;
+
+    // Convert input to long int and double
+    // Use the strto* family of functions to store unwanted text input to a pointer
+    unsigned int duration = strtol(argv[1], &noise, 10);
+    double interval = strtod(argv[2], &noise);
+    FILE *f = fopen("without_gettimeofday.csv", "w");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+
+
+    printf("Duration is %d with an interval of %.2lf between samples\n", duration, interval);
+
+    // Calculated how many entries will be needed for sampling
+    unsigned int timestamp_entries = duration / interval;
+    timestamps = malloc(timestamp_entries * sizeof(unsigned long));
+    for (unsigned int counter_timestamps = 0; counter_timestamps < timestamp_entries; counter_timestamps++) {
+
+        unsigned long time_ms = get_time_ms();
+
+        timestamps[counter_timestamps] = time_ms;
+        counter_timestamps++;
+        // Sleep for ${interval} seconds
+        sleep(interval);
+        fprintf(f, "%ld,", time_ms);
+    }
+    fclose(f);
+
     return 0;
+}
+
+unsigned long get_time_ms() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    // Get the microseconds of the timestamp and convert them to milliseconds
+    unsigned long current_time_us_to_ms = tv.tv_usec / 1000;
+    // Get the seconds of the timestamp, convert them to ms
+    unsigned long current_time_s_to_ms = tv.tv_sec * 1000;
+    // Calc the current timestamp in ms and return it
+    return current_time_s_to_ms + current_time_us_to_ms;
 }
