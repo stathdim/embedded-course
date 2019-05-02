d47334672f2e19480c48c9cf4430f9d67ad2e0e1
Stathis Dimitriadis <stathisdim@gmail.com>
2019-05-02 22:57:05 +0300

    Calculate mean of distances

--- data_handling.py
+++ data_handling.py
@@ -1,7 +1,7 @@
 import plotly.plotly as py
 import plotly.graph_objs as go
 import csv
-
+from statistics import mean 
 
 def read_values(filename):
     timestamps_list = []
@@ -9,12 +9,26 @@ def read_values(filename):
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
-            timestamps_list.append(row[0])
+            #Remove the last el which is '' to avoid failure during typecasting
+            row.pop()
+            for val in row:
+                timestamps_list.append(int(val))
     return timestamps_list
 
 
+def calc_samples_distances(samples):
+    distances = []
+    for i in range(1, len(samples)):
+        diff = samples[i] - samples[i-1]
+        distances.append(diff)
+    return distances
+
 
 timestamps_gettimeofday = read_values("with_gettimeofday.csv")
-timestamps_withoit = read_values("without_gettimeofday.csv")
+timestamps_without = read_values("without_gettimeofday.csv")
 
+distances_gettimeofday = calc_samples_distances(timestamps_gettimeofday)
+distances_without = calc_samples_distances(timestamps_without)
 
+print(mean(distances_without))
+print(mean(distances_gettimeofday))
