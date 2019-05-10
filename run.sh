413c8b2e1ef389027310a3b10b9f2217f86b44d2
Stathis Dim <stathisdim@gmail.com>
2019-05-10 19:35:06 +0300

    new data

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 gcc -std=gnu99 main_with.c -o main_with &&
 gcc -std=gnu99 main_without.c -o main_without &&
-./main_with 5 0.1 &&
-./main_without 5 0.1
+./main_with 7200 0.1 &&
+./main_without 7200 0.1
