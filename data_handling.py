a38f58e136ad0ae830e9cdb3541ac4ca5746b03a
Stathis Dimitriadis <stathisdim@gmail.com>
2019-05-10 17:11:51 +0300

    Create plots

--- data_handling.py
+++ data_handling.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import csv
 from statistics import mean, pstdev, median
 
@@ -26,7 +27,8 @@ timestamps_gettimeofday = read_values("with_gettimeofday.csv")
 timestamps_without = read_values("without_gettimeofday.csv")
 
 
-duration_gettimeofday = (timestamps_gettimeofday[-1] - timestamps_gettimeofday[0])/1000.0
+duration_gettimeofday = (
+    timestamps_gettimeofday[-1] - timestamps_gettimeofday[0])/1000.0
 duration_without = (timestamps_without[-1] - timestamps_without[0])/1000.0
 
 print(f'Duration was {duration_gettimeofday}, {duration_without}')
@@ -54,3 +56,35 @@ print('Standard deviation is', stdev_gettimeofday, stdev_without)
 print('The median is ', median_gettimeofday, median_without)
 print(f'Max difference is {max_gettimeofday} and {max_without}')
 print(f'Min difference is {min_gettimeofday} and {min_without}')
+
+plt.figure(1, figsize=(14, 10))
+plt.plot(distances_gettimeofday, '.')
+plt.savefig('distances.png', bbox_inches='tight')
+
+names = ['using gettimeofday', 'without gettimeofday']
+
+plt.figure(2, figsize=(7, 5))
+plt.scatter(names, [mean_gettimeofday, mean_without])
+plt.suptitle('Mean')
+plt.savefig('mean.png', bbox_inches='tight')
+
+plt.figure(3, figsize=(7, 5))
+plt.scatter(names, [median_gettimeofday, median_without])
+plt.suptitle('Median')
+plt.savefig('median.png', bbox_inches='tight')
+
+
+plt.figure(4, figsize=(7, 5))
+plt.scatter(names, [stdev_gettimeofday, stdev_without])
+plt.suptitle('Standard Deviation')
+plt.savefig('stdev.png', bbox_inches='tight')
+
+plt.figure(5, figsize=(7, 5))
+plt.scatter(names, [max_gettimeofday, max_without])
+plt.suptitle('Max')
+plt.savefig('max.png', bbox_inches='tight')
+
+plt.figure(6, figsize=(7, 5))
+plt.scatter(names, [min_gettimeofday, min_without])
+plt.suptitle('Min')
+plt.savefig('min.png', bbox_inches='tight')
